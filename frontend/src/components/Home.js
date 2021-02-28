4ef6f751f21bbc72b1bb8756af6112da7416dd9b
Hoang An Huynh <anb1706560@student.ctu.edu.vn>
2021-02-28 20:47:42 +0700

    filter_price

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -6,6 +6,9 @@ import React,
 } from 'react';
 import Pagination from 'react-js-pagination';
 
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css';
+
 import MetaData from './layouts/MetaData';
 import Product from './product/Product';
 import Loader from './layouts/Loader';
@@ -21,12 +24,15 @@ import {
     getProducts
 } from '../actions/product.actions';
 
-
+const { createSliderWithTooltip } = Slider;
+const Range = createSliderWithTooltip(Slider.Range);
 
 
 const Home = ({ match }) => {
 
     const [currentPage, setCurrentPage] = useState(1)
+    const [price, setPrice] = useState([20000, 100000])
+
     const alert = useAlert();
     const dispatch = useDispatch();
 
@@ -43,9 +49,9 @@ const Home = ({ match }) => {
     useEffect(() => {
         if(error) return alert.error(error);
 
-        dispatch(getProducts(keyword, currentPage));
+        dispatch(getProducts(keyword, currentPage, price));
 
-    }, [dispatch, alert, error, keyword, currentPage])
+    }, [dispatch, alert, error, keyword, currentPage, price])
 
     function setCurrentpageNo(pageNumber) {
         setCurrentPage(pageNumber)
@@ -57,13 +63,48 @@ const Home = ({ match }) => {
                 <Fragment>
                     <MetaData title={'Trang web mua sắm hàng đầu Việt Nam'} />
 
-                    <h1 id="products_heading">Sản phẩm gần đây</h1>
+                    {/* <h1 id="products_heading">Sản phẩm gần đây</h1> */}
                     <section id="products" className="container mt-5">
-                        <div className="row">
-                            {products && products.map(product => (
-                                <Product key={product._id} product={product} />
-                            ))}
-                        </div>
+                    <div className="row">
+
+                    {keyword ? (
+                        <Fragment>
+                            <div className="col-6 col-md-3 mt-5 mb-5">
+                                <div className="px-5">
+                                    <Range
+                                        marks={{
+                                            20000: `20000đ`,
+                                            100000: `100000đ`
+                                        }}
+                                        min={20000}
+                                        max={100000}
+                                        defaultValue={[20000, 100000]}
+                                        tipFormatter={value => `${value}`}
+                                        tipProps={{
+                                            placement: "top",
+                                            visible: true
+                                        }}
+                                        value={price}
+                                        onChange={price => setPrice(price)}
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="col-6 col-md-9">
+                                <div className="row">
+                                    {products.map(product => (
+                                        <Product key={product._id} product={product} col={4} />
+                                    ))}
+                                </div>
+                            </div>
+                        </Fragment>
+                    ) : (
+                            products.map(product => (
+                                <Product key={product._id} product={product} col={3} />
+                            ))
+                        )}
+
+                </div>
                     </section>
 
                     {resPerPage <= productCount &&(
